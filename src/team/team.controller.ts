c24f204a4e90080e3a1099b636f1e6f5cb6deb91
visalrotanak <visalrotanak7@gmail.com>
2024-10-04 08:59:01 +0700

    p0

--- src/team/team.controller.ts
+++ src/team/team.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { TeamService } from './team.service';
+import { TeamDto } from './dto';
+import { CurrentUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtGuard, RolesGuard } from 'src/auth/guard';
 
+@ApiTags('Team')
+@UseGuards(JwtGuard, RolesGuard)
 @Controller('team')
 export class TeamController {
   constructor(private TeamService: TeamService) {}
+
+  @Post()
+  createTeam(@Body() dto: TeamDto, @CurrentUser() user: User) {
+    return this.TeamService.createTeam(dto, user);
+  }
+
+  @Patch('join/:id')
+  joinTeam(@Param('id') id: number, @CurrentUser() user: User) {
+    return this.TeamService.joinTeam(id, user);
+  }
+
+  @Patch('update/:id')
+  updateTeam(
+    @Param('id') id: number,
+    @Body() dto: TeamDto,
+    @CurrentUser() user: User,
+  ) {
+    return this.TeamService.updateTeam(id, dto, user);
+  }
+
+  @Get()
+  getTeams(@CurrentUser() user: User) {
+    return this.TeamService.getTeams(user);
+  }
 }
--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -4,4 +4,81 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class TeamService {
   constructor(private prisma: PrismaService) {}
+
+  async createTeam(dto, user) {
+    try {
+      const team = await this.prisma.team.create({
+        data: {
+          name: dto.name,
+          teamOwnerId: user.id,
+          createdAt: new Date(),
+        },
+      });
+
+      return {
+        status: 201,
+        message: 'created successfullt',
+        data: team,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async joinTeam(id, user) {
+    try {
+      const team = await this.prisma.team.update({
+        where: {
+          id: id,
+        },
+        data: {
+          members: user,
+        },
+      });
+      return {
+        status: 200,
+        message: 'joined successfully',
+        data: team,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateTeam(id, dto, user) {
+    try {
+      const team = await this.prisma.team.update({
+        where: {
+          id: id,
+        },
+        data: {
+          ...dto,
+        },
+      });
+
+      return {
+        status: 200,
+        message: 'updated successfullt',
+        data: team,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getTeams(user) {
+    try {
+      const teams = await this.prisma.team.findMany({
+        where: {
+          teamOwnerId: user.id,
+        },
+      });
+      return {
+        status: 200,
+        data: teams,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
 }
